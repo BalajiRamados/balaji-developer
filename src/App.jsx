5ace204d7d99a66c1b3606581fe5e805dc4ee3c7
balaji <balaji.r.772000@gmail.com>
2025-07-05 22:25:24 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from "./Home"
+
+const App = () => {
+  
+
+  return (
+    <>
+      <Home />
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1>Hello World</h1>
+            <p>Welcome to my portfolio!</p>
+        </div>
+    )
+}
+
+
+export default Home;
